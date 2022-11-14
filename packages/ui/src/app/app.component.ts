3c4200f4934154ff2ac78ce23323fcdb0f842b0e
Daniel Jiménez O <dany.jimenez999@gmail.com>
2022-11-14 06:07:56 -0500

    Material

--- packages/ui/src/app/app.component.ts
+++ packages/ui/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'T M Simple';
+  title = 'ui';
 }
--- packages/ui/src/app/app.module.ts
+++ packages/ui/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateTestcaseComponent } from './create-testcase/create-testcase.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateTestcaseComponent
+	CreateTestcaseComponent,
+    AppComponent
   ],
   imports: [
+	MatDividerModule,
+	MatCardModule,
+	MatSelectModule,
+	MatButtonModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
